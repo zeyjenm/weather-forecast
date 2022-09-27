7327ddd617e7550f362e7712e67a2f4996f64fe5
Zeynep <zeynepmarch@gmail.com>
2022-09-27 12:18:32 +1000

    Remove fetch for weather status description

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const currentTempOutput = document.querySelector('#current-temp');
 const minTempOutput = document.querySelector('#min-temp');
 const maxTempOutput = document.querySelector('#max-temp');
 const weatherStatusOutput = document.querySelector('#status');
-const weatherDescriptionOutput = document.querySelector('#description');
 const windSpeedOutput = document.querySelector('#wind-speed');
 const windDirectionOutput = document.querySelector('#wind-direction');
 
@@ -19,17 +18,18 @@ function getWeatherData (city) {
         let minTemp = Math.floor(weatherData.main.temp_min - 273.15);
         let maxTemp = Math.ceil(weatherData.main.temp_max - 273.15);
         let status = weatherData.weather[0].main;
-        let description = weatherData.weather[0].description;
         let windDirection = weatherData.wind.deg;
         let windSpeed = weatherData.wind.speed;
         currentTempOutput.textContent += currentTemp;
         minTempOutput.textContent += minTemp;
         maxTempOutput.textContent += maxTemp;
         weatherStatusOutput.textContent += status;
-      //  weatherDescriptionOutput.textContent += description;
         windSpeedOutput.textContent += windSpeed + ' knots';
         windDirectionOutput.textContent += windDirection + ' deg';
     });
+    return {
+        currentTemp,
+    }
 }
 
 
